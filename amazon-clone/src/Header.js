8c5b8df122d9615ebf064fc92afde44dbc2bd6b9
Benjamin Huang <bhuang@rms.com>
2020-07-26 10:51:14 -0700

    header and nav

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 //
 import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 import "./Header.css";
 
@@ -14,6 +15,34 @@ function Header() {
       <div className="header__search">
         <input type="text" className="header__searchInput" />
         <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className="header__nav">
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello User</span>
+            <span className="header__optionLineTwo">Sign In</span>
+          </div>
+        </Link>
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hddddd</span>
+            <span className="header__optionLineTwo">Hddddd</span>
+          </div>
+        </Link>
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hddddd</span>
+            <span className="header__optionLineTwo">Hddddd</span>
+          </div>
+        </Link>
+
+        <Link to="/checkout" className="header__link">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon/>
+            <span className="header__optionLineTwo header__basketount">0</span>
+          </div>
+        </Link>
       </div>
     </nav>
   );
