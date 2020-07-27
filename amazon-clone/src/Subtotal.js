12d4b601d9426660424e226b7b9bd77c1e3069e7
Benjamin Huang <bhuang@rms.com>
2020-07-26 19:02:47 -0700

    subtotal

--- amazon-clone/src/Subtotal.js
+++ amazon-clone/src/Subtotal.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React from "react";
 //
+import CurrencyFormat from "react-currency-format";
+// Context
+import { useStateValue } from "./StateProvider";
+import {getBasketTotal} from "./reducer";
 //
+import "./Subtotal.css";
 
 function Subtotal() {
+  const [{ basket }] = useStateValue();
+
   return (
-    <div>
-      Subtotal
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText = {
+          (value) =>(
+            <>
+              <p>
+          Subtotal({basket.length} itmes) : <strong>{``}</strong>
+              </p>
+              <small className="subtotal__gift">
+                <input type="checkbox" /> This order contains a gift
+              </small>
+            </>
+          )
+        }
+        decimalScale={2}
+        value={getBasketTotal(basket)}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+      <button>Proceed to Checkout</button>
     </div>
-  )
+  );
 }
 
-export default Subtotal
+export default Subtotal;
