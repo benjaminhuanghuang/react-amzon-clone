f850bc7999b8121f2f3ee0cb1a77b19d4fb088ca
Benjamin Huang <bhuang@rms.com>
2020-07-26 18:37:04 -0700

    subtotal for checkout

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -3,6 +3,7 @@ import React from "react";
 import { useStateValue } from "./StateProvider";
 //
 import CheckoutProduct from "./CheckoutProduct";
+import SubTotal from "./Subtotal";
 
 import "./Checkout.css";
 
@@ -11,30 +12,39 @@ function Checkout() {
 
   return (
     <div className="checkout">
-      <img
-        className="checkout__ad"
-        src="https://www.publicdomainpictures.net/pictures/150000/velka/banner-header-tapete-145002399028x.jpg"
-        alt=""
-      />
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://www.publicdomainpictures.net/pictures/150000/velka/banner-header-tapete-145002399028x.jpg"
+          alt=""
+        />
 
-      {basket?.length === 0 ? (
-        <div>
-          <h2>Your shopping basket is empty</h2>
-          <p> You have no itmes in your basket.</p>
-        </div>
-      ) : (
-        <div>
-          {basket.map((item) => (
-            <CheckoutProduct
-              id={item.id}
-              title={item.title}
-              image={item.image}
-              price={item.price}
-              rating={item.rating}
-            ></CheckoutProduct>
-          ))}
-        </div>
-      )}
+        {basket?.length === 0 ? (
+          <div>
+            <h2>Your shopping basket is empty</h2>
+            <p> You have no itmes in your basket.</p>
+          </div>
+        ) : (
+          <div>
+            <h2 className="checkout__title">Your Shopping Basket</h2>
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              ></CheckoutProduct>
+            ))}
+          </div>
+        )}
+      </div>
+
+      {basket?.length > 0 && <div className="checkout__right">
+        <SubTotal>
+
+        </SubTotal>
+        </div>}
     </div>
   );
 }
---
+++ amazon-clone/src/Subtotal.js
@@ -0,0 +1,13 @@
+import React from 'react'
+//
+//
+
+function Subtotal() {
+  return (
+    <div>
+      Subtotal
+    </div>
+  )
+}
+
+export default Subtotal
